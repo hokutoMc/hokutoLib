f909e1adeb1d02e9bcee83b815373e44f9ee97eb
hokuto <youshouldseealcor@gmail.com>
2015-03-20 22:57:59 +0900

    deleted test code

--- src/main/scala/com/github/hokutomc/lib/scala/Mod_HTLibScala.scala
+++ src/main/scala/com/github/hokutomc/lib/scala/Mod_HTLibScala.scala
@@ -1,23 +1,7 @@
 package com.github.hokutomc.lib.scala
 
-import com.github.hokutomc.lib.item.{HT_Item, HT_ItemStackBuilder}
-import com.github.hokutomc.lib.scala.HT_ScalaConversion._
-import net.minecraft.init.Items
 import net.minecraftforge.fml.common.Mod
-
-import scala.collection.JavaConversions
 
 @Mod(modid = "hokutomc.lib.scala", modLanguage = "scala")
 object Mod_HTLibScala {
-
-  val a: HT_ItemStackBuilder[_] = Items.diamond
-
-  val b = new MyItem
-
-  class MyItem extends HT_Item("a", "b") {
-    this.m_subItems = JavaConversions.seqAsJavaList(Seq.tabulate(9) { i =>
-      val c: HT_ItemStackBuilder.Raw = this
-      c
-    })
-  }
 }
