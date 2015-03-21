b21f4bb4c070675a7906f2645225fc81362d714b
hokuto <youshouldseealcor@gmail.com>
2015-03-21 13:07:28 +0900

    bugfix

--- src/main/scala/com/github/hokutomc/lib/scala/nbt/HT_NBTKey.scala
+++ src/main/scala/com/github/hokutomc/lib/scala/nbt/HT_NBTKey.scala
@@ -5,7 +5,7 @@ package com.github.hokutomc.lib.scala.nbt
  */
 class HT_NBTKey(val string: String) extends AnyVal {
 
-  def :=[A <: HT_T_NBTCompound, T <: HT_T_NBTValue[T]](nbtValue: T)(implicit a: A) = {
+  def :=[A <: HT_T_NBTCompound[_], T <: HT_T_NBTValue[T]](nbtValue: T)(a: A) = {
     a.apply[T](string, nbtValue)
   }
 }
